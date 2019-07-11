82cc02135a83f5eac98dbd779e4cafe308b28b4c
Dexter Yang <dexter.yang@qq.com>
2019-07-11 13:16:58 +0800

    Initial commit

---
+++ src/main/kotlin/net/gridtech/machinecontroller/MachineControllerApplication.kt
@@ -0,0 +1,11 @@
+package net.gridtech.machinecontroller
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MachineControllerApplication
+
+fun main(args: Array<String>) {
+    runApplication<MachineControllerApplication>(*args)
+}
